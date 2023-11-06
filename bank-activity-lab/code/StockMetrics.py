51fd1d857800b737f852421cf4203936acfd10c9
vraysacee11 <143121928+vraysacee11@users.noreply.github.com>
2023-11-06 12:24:29 -0800

    Add files via upload

--- bank-activity-lab/code/StockMetrics.py
+++ bank-activity-lab/code/StockMetrics.py
@@ -26,16 +26,35 @@ class StockMetrics(StockData):
                 except ValueError: 
                     continue
             average = stats.mean(y)
-            rounded_average = rounded(average, 3)
+            rounded_average = round(average, 3)
             averages.append(rounded_average)
         return averages
 
     def median02(self):
         """pt2
         """
-        ...
+        medians = []
+        for row in self.data:
+            data_row = row[1:]
+            y = [float(x) for x in data_row if x != ""]
+            if y: 
+                median = stats.median(y)
+                medians.append(median)
+        return medians 
+
 
     def stddev03(self):
         """pt3
         """
-        ...
+        standard_deviations = []
+        for row in self.data:
+            data_row = row[1:]
+            y = [float(x) for x in data_row if x != ""]
+            if y:
+                if len(y) > 1:
+                   std_dev = stats.stdev(y)
+                   rounded_std_dev = round(std_dev, 3)
+                   standard_deviations.append(rounded_std_dev)
+        return standard_deviations
+                 
+
